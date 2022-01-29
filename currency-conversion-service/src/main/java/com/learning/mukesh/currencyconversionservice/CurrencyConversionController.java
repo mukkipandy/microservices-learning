250689e32adb53fdb485c5a324f5cc563135262c
Mukesh Pandey <mukesh.mtr@gmail.com>
2022-01-30 01:30:16 +0530

    first commit api gateway

--- currency-conversion-service/src/main/java/com/learning/mukesh/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/learning/mukesh/currencyconversionservice/CurrencyConversionController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+//@RequestMapping("/currency-conversion")
 public class CurrencyConversionController {
 
 	@Autowired
