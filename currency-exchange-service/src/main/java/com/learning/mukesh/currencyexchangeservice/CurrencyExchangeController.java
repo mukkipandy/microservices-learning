1f725d3fa608133c699f31c9b9a92128f48a2750
Mukesh Pandey <mukesh.mtr@gmail.com>
2022-01-29 17:12:36 +0530

    addming currency conversion

--- currency-exchange-service/src/main/java/com/learning/mukesh/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/mukesh/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.learning.mukesh.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	private Environment environment;
+
+	@Autowired
+	private CurrencyExchangeRepository repository;
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
-		CurrencyExchange currencyExchange = new CurrencyExchange(100L, from, to, BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		if (currencyExchange == null) {
+			throw new RuntimeException("Invalid conversion from: " +from+ " to: " + to);
+		}
+		currencyExchange.setEnvironment(environment.getProperty("server.port"));
 
 		return currencyExchange;
 
---
+++ currency-exchange-service/src/main/java/com/learning/mukesh/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,8 @@
+package com.learning.mukesh.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange, Long> {
+	
+	CurrencyExchange findByFromAndTo(String from, String to);
+}
